186b8d2c7c2194aaff195e1820430b790da15808
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-07-18 01:05:24 +0600

    Frontend added

--- HexaProject/HexaApp/urls.py
+++ HexaProject/HexaApp/urls.py
@@ -4,5 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home ,name='home'),
+    path('about/',views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('form/', views.form, name='form')
+
 ]
--- HexaProject/HexaApp/views.py
+++ HexaProject/HexaApp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.shortcuts import render, redirect
+
+# Create your views here.
 def home(request):
-    return render(request , 'home.html')
+    return render(request , 'Home.html')
+
+def about(request):
+    return render(request,'About.html')
+
+def contact(request):
+    return render(request,'Contact.html')
+
+def form(request):
+    return render(request,'Form.html')
