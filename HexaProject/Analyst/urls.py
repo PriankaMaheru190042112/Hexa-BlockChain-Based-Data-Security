d9b4f7ab80d8d6f71d375fbc952eb30a9f4bcbec
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-07-19 00:28:33 +0600

    Analyst View PDF Template

--- HexaProject/Analyst/urls.py
+++ HexaProject/Analyst/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.analyst_home, name= 'analyst_home'),
+    path('', views.analyst_home, name= 'analyst'),
+    path('viewPDF/', views.view_pdf),
  
 ]
--- HexaProject/Analyst/views.py
+++ HexaProject/Analyst/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def analyst_home(request):
     return render (request,'Analyst/AnalystHome.html')
+
+def view_pdf(request):
+    return render (request, 'Analyst/ViewPDF.html')
