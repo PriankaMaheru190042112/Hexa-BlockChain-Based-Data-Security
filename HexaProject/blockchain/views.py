036732f0295da1680e478ceadb75b73b797f4b89
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-07-19 17:10:06 +0600

    BlockChain Integration

--- HexaProject/blockchain/views.py
+++ HexaProject/blockchain/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render
-from .ledger import Blockchain, block
+from .models import Blockchain
+
 
 
 # Create your views here.
 def ledger(request):
-    bc=Blockchain()
-    bc.add_block('1')
-    bc.add_block('2')
-    bc.add_block('3')
-
-    for block in bc.chain:
-        print(block.__dict__)
-    return render(request, 'blockchain/ledger.html')
+    blocks = Blockchain.objects.all()
+    return render(request, 'blockchain/ledger.html', {'blocks' : blocks})
 
 def logbook(request):
     return render(request, 'blockchain/logbook.html')
-
