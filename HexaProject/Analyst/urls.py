08093fdcc3135f719430367304bf8dd9d767cd6d
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-07-20 04:50:59 +0600

    download files

--- HexaProject/Analyst/urls.py
+++ HexaProject/Analyst/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.analyst_home, name= 'analyst'),
     path('viewPDF/', views.view_pdf),
     path('permittedFiles/', views.permitted_files),
+    path('download/', views.download_file, name='download_file'),
  
 ]
--- HexaProject/Analyst/views.py
+++ HexaProject/Analyst/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import ReadOnlyDocument
+from django.shortcuts import get_object_or_404
+import os
+from django.http import HttpResponse
 
 # Create your views here.
 def analyst_home(request):
@@ -14,3 +17,16 @@ def permitted_files(request):
     files = ReadOnlyDocument.objects.filter(reader=user)
     print(files)
     return render (request, 'Analyst/PermittedFiles.html', {'files':files})
+
+
+def download_file(request):
+    document = ReadOnlyDocument.objects.filter(reader=request.user)
+    print('document',document[len(document)-1].file)
+    file_path = str(document[len(document)-1].file)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            response = HttpResponse(file.read(), content_type='application/octet-stream')
+            response['Content-Disposition'] = f'attachment; filename="{os.path.basename(file_path)}"'
+            return response
+
+    return HttpResponse("File not found.", status=404)
